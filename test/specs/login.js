5788aec3c792f427675fa464108e0a74813aa4aa
Vadim Volkov <triv@me.com>
2020-01-14 22:08:06 -0500

    Createt login page init test

--- test/specs/login.js
+++ test/specs/login.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 
 const email = 'vwolkof@yahoo.com';
-import{password} from './register';
+//import{password} from 'register';
 
 
 
@@ -12,7 +12,8 @@ describe('login page - Login', () => {
   });
 
   it('should have the right title', () => {
-    const actual = browser.getTitle();
+    const actual = $('h1').getText();
+    console.log(actual);
     const expected = 'User Login';
     expect(actual).equal(expected);
   });
