ba21d632f837d4f4c43fab6c39b37052e4547d67
Vadim Volkov <triv@me.com>
2020-01-14 22:02:18 -0500

    Createt login page init test

---
+++ test/specs/login.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai');
+
+const email = 'vwolkof@yahoo.com';
+import{password} from './register';
+
+
+
+
+describe('login page - Login', () => {
+  before(() => {
+    browser.url('https://stage.pasv.us/user/login');
+  });
+
+  it('should have the right title', () => {
+    const actual = browser.getTitle();
+    const expected = 'User Login';
+    expect(actual).equal(expected);
+  });
+
+
+});
